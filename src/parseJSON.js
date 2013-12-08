41c1512a1363bbad1527b36ffe06256824aa10e6
wjmao88 <wjmao88@gmail.com>
2013-12-08 18:08:34 +0800

    added skeleton for parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,40 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+	if (json.charAt(0) != '{' || json.charAt(json.length-1) != '}'){
+		throw('missing brackets');
+	}
+  switch(jsonType){
+  	case 'map':
+  	case 'array':
+  	case 
+  }
 };
+
+function jsonType(json){
+	if (json.charAt(''))
+}
+
+function jsonObject(json){
+
+}
+
+function jsonArray(json){
+
+}
+
+function jsonString(json){
+
+}
+
+function jsonNumber(json){
+
+}
+
+function jsonBoolean(json){
+
+}
+
+function jsonNull(json){
+	
+}
