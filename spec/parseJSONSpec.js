1608eb5a48703aab16c5d7836b3c530c79bf5733
wjmao88 <wjmao88@gmail.com>
2014-01-19 15:00:06 -0500

    reset spec files

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -8,18 +8,17 @@ describe("parseJSON", function(){
       expect(equality).toBeTruthy();
     });
 
-    // extraCreditStrings.forEach(function(string){
-    //   log(string);
-    //   var expected = JSON.parse(string);
-    //   var result = parseJSON(string);
-    //   var equality = _.isEqual(result, expected);
-    //   expect(equality).toBeTruthy();
-    // });
+    extraCreditStrings.forEach(function(string){
+      var expected = JSON.parse(string);
+      var result = parseJSON(string);
+      var equality = _.isEqual(result, expected);
+      expect(equality).toBeTruthy();
+    });
   });
 
   it("should error out sometimes", function(){
     arrayWithInvalidStrings.forEach(function(test){
-      // expect(parseJSON(test)).toBe(undefined); // you can use this test if you'd prefer
+      //expect(parseJSON(test)).toBe(undefined); // you can use this test if you'd prefer
       expect(function(){
         parseJSON(test);
       }).toThrow();
